ed3fc6c866b4a01fe74b78c566b7eb9e9e242342
pradeep9958586878 <pradeepj0508@gmail.com>
2022-06-13 11:29:34 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const moment = require('moment')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+let time= moment();
+app.use (function(req, res, next){
+    
+    let id= req.ip
+    let rt= req.originalUrl
+    console.log(time.format('dd:mm:yyyy'))
+     console.log(time,id,rt)
+    next()
+    
+})
 
 
 mongoose.connect("mongodb+srv://pradeepj0508:pradeepj0508@cluster0.jlwna.mongodb.net/List", {
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,15 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const authorController= require("../controllers/authorController")
-const bookController= require("../controllers/bookController")
-const publisherController=require("../controllers/publisherController")
 
 
 
-router.post("/createAuthor", authorController.createAuthor)
-router.post("/createPublisher",publisherController.createPublisher)
-router.post("/createBook",bookController.createBook)
-router.get("/getAllBooks",bookController.getAllBooks)
+
+router.post("/createAuthor", , authorController.createAuthor)
 
 
 module.exports = router;
